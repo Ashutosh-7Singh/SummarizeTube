38316ae010091507d9286cc81e16bd105172c757
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-04-23 12:07:42 +0530

    lecture 2 is compelted

--- SummarizeTube/frontend/src/app/page.tsx
+++ SummarizeTube/frontend/src/app/page.tsx
@@ -1,24 +1,52 @@
+import { HeroSection } from "@/components/custom/HeroSection";
 import { Button } from "@/components/ui/button";
+import { flattenAttributes } from "@/lib/utils";
 import Image from "next/image";
+import QueryString from "qs";
 
-async function getStrapiData(path:string){
-  const baseUrl="http://localhost:1337";
+
+
+
+const homePageQuery = QueryString.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+  console.log(url.href);
+
   try {
-    const response=await fetch(baseUrl + path)
-    const data=await response.json();
-    return data
+    const response = await fetch(url.href,{cache:'no-store'});
+    const data = await response.json();
+    const flattenedData = flattenAttributes(data);
+    console.dir(flattenedData, { depth: null });
+    return flattenedData;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 export default async function Home() {
-  const strapiData=await getStrapiData("/api/home-page");
+  const strapiData = await getStrapiData("/api/home-page");
   console.log(strapiData);
-  const {title,description}=strapiData.data.attributes;
+  const { title, description,blocks } = strapiData;
   return (
-  <main className="container mx-auto py-6">
-    <h1 className="text-5xl font-bold">{title}</h1>
-  <p className="text-xl mt-4">{description}</p>
-  </main>
+    <main>
+      {/* <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p> */}
+      <HeroSection data={blocks[0]}/>
+    </main>
   );
 }
