f41207a45238b0dfc992b4dbcf5dfeb06e7013cc
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-04-23 12:42:27 +0530

    3.1 image is completed

--- SummarizeTube/frontend/src/components/custom/HeroSection.tsx
+++ SummarizeTube/frontend/src/components/custom/HeroSection.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { StrapiImage } from "@/components/custom/StrapiImage";
 interface ImageProps {
   id: number;
   url: string;
@@ -24,20 +25,17 @@ export function HeroSection({ data }: Readonly<HeroSectionProps>) {
   console.dir(data, { depth: null });
   const { heading, subHeading, image, link } = data;
   console.log(image);
+  
   //   const imageURL="http://localhost:1337"+ image.url;
-  const imageURL = `http://localhost:1337${image.data[0].url}`; // Assuming first element
+  //   const imageURL = `http://localhost:1337${image.data[0].url}`; // Assuming first element
 
   return (
     <header className="relative h-[600px] overflow-hidden">
-      <img
+      <StrapiImage
         alt="Background"
         className="absolute inset-0 object-cover w-full h-full"
         height={1080}
-        src={imageURL}
-        style={{
-          aspectRatio: "1920/1080",
-          objectFit: "cover",
-        }}
+        src={image?.data?.[0]?.url} 
         width={1920}
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
